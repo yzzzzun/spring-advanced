6223c8acf629c48099295ecd28ebb8710aca1b43
yzzzzun <yzzzzun@gmail.com>
2023-01-04 20:14:04 +0900

    feat: implements log trace

---
+++ advanced/src/main/java/com/yzzzzun/advanced/trace/TraceStatus.java
@@ -0,0 +1,16 @@
+package com.yzzzzun.advanced.trace;
+
+import lombok.Getter;
+
+@Getter
+public class TraceStatus {
+	private TraceId traceId;
+	private Long startTimeMs;
+	private String message;
+
+	public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+		this.traceId = traceId;
+		this.startTimeMs = startTimeMs;
+		this.message = message;
+	}
+}
