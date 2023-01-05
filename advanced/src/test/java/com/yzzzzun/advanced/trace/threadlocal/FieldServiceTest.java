2a627f139f1a0f27e47919452b6c39010b29cf00
yzzzzun <yzzzzun@gmail.com>
2023-01-05 21:58:26 +0900

    teat: 동시성 문제 발생 테스트

---
+++ advanced/src/test/java/com/yzzzzun/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,47 @@
+package com.yzzzzun.advanced.trace.threadlocal;
+
+import org.junit.jupiter.api.Test;
+
+import com.yzzzzun.advanced.trace.threadlocal.code.FieldService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldServiceTest {
+
+	private FieldService fieldService = new FieldService();
+
+	@Test
+	void field() {
+		log.info("main start");
+		Runnable userA = () -> {
+			fieldService.logic("userA");
+		};
+
+		Runnable userB = () -> {
+			fieldService.logic("userB");
+		};
+
+		Thread threadA = new Thread(userA);
+		threadA.setName("thread-A");
+		Thread threadB = new Thread(userB);
+		threadB.setName("thread-B");
+
+		threadA.start();
+		// sleep(2000);	//동시성 문제 발생 안함.
+		sleep(100);    //동시성 문제 발생.
+
+		threadB.start();
+
+		sleep(3000);
+		log.info("main exit");
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
