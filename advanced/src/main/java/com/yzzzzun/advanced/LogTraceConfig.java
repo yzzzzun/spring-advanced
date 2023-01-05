713246099615c4cfe1febaa2c951a2eaede9c5d8
yzzzzun <yzzzzun@gmail.com>
2023-01-05 22:17:26 +0900

    feat: threadLocal log trace구현

--- advanced/src/main/java/com/yzzzzun/advanced/LogTraceConfig.java
+++ advanced/src/main/java/com/yzzzzun/advanced/LogTraceConfig.java
@@ -3,14 +3,14 @@ package com.yzzzzun.advanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.yzzzzun.advanced.trace.logtrace.FieldLogTrace;
 import com.yzzzzun.advanced.trace.logtrace.LogTrace;
+import com.yzzzzun.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
 	@Bean
 	public LogTrace logTrace() {
-		return new FieldLogTrace();
+		return new ThreadLocalLogTrace();
 	}
 }
